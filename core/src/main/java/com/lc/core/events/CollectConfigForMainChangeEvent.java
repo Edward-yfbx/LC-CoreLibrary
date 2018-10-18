6bb29fc382c57254e3fd9cdbdce98a3784b8daf1
Edward <yfbx0416@163.com>
2018-10-18 11:35:50 +0800

    update

---
+++ core/src/main/java/com/lc/core/events/CollectConfigForMainChangeEvent.java
@@ -0,0 +1,11 @@
+package com.lc.core.events;
+
+/**
+ * Author: Edward
+ * Date: 2018/7/5
+ * Description:采集配置变更事件
+ */
+
+
+public class CollectConfigForMainChangeEvent {
+}
---
+++ core/src/main/java/com/lc/core/events/UploadConfigChangeEvent.java
@@ -0,0 +1,13 @@
+package com.lc.core.events;
+
+/**
+ * Author: Edward
+ * Date: 2018/7/18
+ * Description:上传配置变更事件
+ */
+
+
+public class UploadConfigChangeEvent {
+
+
+}
